26cbe1117888bf0e029d9893337fa9cddab0b14c
Kayla Webb <kaylarwebb@gmail.com>
2017-11-01 12:19:54 -0400

    Completed first drill

---
+++ object_exercises2.js
@@ -0,0 +1,12 @@
+const loaf = {
+    flour: 300,
+    water: 210,
+    hydration: function(){
+        return this.water/this.flour*100;
+    }
+}
+
+console.log(loaf.flour);
+console.log(loaf.water);
+
+console.log(loaf.hydration());
