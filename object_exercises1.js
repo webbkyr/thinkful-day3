61b525ca5f74d5c00186df07ad7d868c23d8b358
Ryan Sabo <sabora920@gmail.com>
2017-11-01 12:06:14 -0400

    added delete keys drill

--- object_exercises1.js
+++ object_exercises1.js
@@ -98,50 +98,111 @@
   
 // })();
 
-function personMaker() {
-    const person = {
-      firstName: 'Paul',
-      lastName: 'Jones',
-      // replace `null` with a function that uses self reference to return
-      // full name
-      fullName: function() {
-          return `${this.firstName} ${this.lastName}`;
-      }
-    };
-    return person;
+// function personMaker() {
+//     const person = {
+//       firstName: 'Paul',
+//       lastName: 'Jones',
+//       // replace `null` with a function that uses self reference to return
+//       // full name
+//       fullName: function() {
+//           return `${this.firstName} ${this.lastName}`;
+//       }
+//     };
+//     return person;
+//   }
+  
+  
+  
+//   /* From here down, you are not expected to 
+//      understand.... for now :)  
+     
+     
+//      Nothing to see here!
+     
+//   */
+  
+//   (function testPersonMaker() {
+//     const person = personMaker();
+//     if (typeof person !== 'object') { 
+//       console.error('ERROR: `personMaker` must return an object');
+//       return false
+//     }
+//     if (typeof person.fullName !== 'function') {
+//       console.error('ERROR: `fullName` must be a method');
+//       return false
+//     }
+//     if (person.fullName() !== 'Paul Jones') {
+//       console.error(`ERROR: The value for \`fullName\` should be "Paul Jones" but was ${person.fullName()}`);
+//       return false;
+//     }
+//     person.firstName = 'Lisa';
+//     person.lastName = 'Simpson';
+//     if (person.fullName() !== 'Lisa Simpson') {
+//       console.error(
+//         `\`personMaker\` is not using self reference correctly. When firstName set to "Lisa" and lastName set to "Simpson", it should return "Lisa Simpson" but returned ${person.fullName()}`
+//       );
+//     }
+//     console.log('SUCCESS: `personMaker` works correctly!');
+    
+//   })();
+
+
+
+
+
+
+function keyDeleter(obj) {
+  // your code here
+  delete obj.foo;
+  delete obj.bar;
+
+  return obj;
+}
+
+
+var sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang'
+};
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+(function testKeyDeleter() {
+  var obj = keyDeleter({
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang'
+  });
+  
+  if (typeof obj !== 'object') {
+    console.error('ERROR: `keyDeleter` must be return an object');
+    return false
   }
-  
-  
-  
-  /* From here down, you are not expected to 
-     understand.... for now :)  
-     
-     
-     Nothing to see here!
-     
-  */
-  
-  (function testPersonMaker() {
-    const person = personMaker();
-    if (typeof person !== 'object') { 
-      console.error('ERROR: `personMaker` must return an object');
-      return false
-    }
-    if (typeof person.fullName !== 'function') {
-      console.error('ERROR: `fullName` must be a method');
-      return false
-    }
-    if (person.fullName() !== 'Paul Jones') {
-      console.error(`ERROR: The value for \`fullName\` should be "Paul Jones" but was ${person.fullName()}`);
+  ['foo', 'bar'].forEach(function(key) {
+    if (key in obj) {
+      console.error('`keyDeleter` did not delete the key for ' + key);
       return false;
     }
-    person.firstName = 'Lisa';
-    person.lastName = 'Simpson';
-    if (person.fullName() !== 'Lisa Simpson') {
-      console.error(
-        `\`personMaker\` is not using self reference correctly. When firstName set to "Lisa" and lastName set to "Simpson", it should return "Lisa Simpson" but returned ${person.fullName()}`
-      );
+  });
+  ['bizz', 'bang'].forEach(function(key) {
+    if (!(key in obj && obj[key] === key)) {
+      console.error('`keyDeleter` is deleting keys other than `foo` and `bar`');
+      return false;
     }
-    console.log('SUCCESS: `personMaker` works correctly!');
-    
-  })();
+  });
+  console.log('SUCCESS: `keyDeleter` works correctly!');
+  
+})();
+
+
+
