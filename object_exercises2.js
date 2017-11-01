cfe15bff0f6c2caed4b405ad2379edc6e4b12dff
Ryan Sabo <sabora920@gmail.com>
2017-11-01 12:30:57 -0400

    added next challenge

--- object_exercises2.js
+++ object_exercises2.js
@@ -1,12 +1,24 @@
-const loaf = {
-    flour: 300,
-    water: 210,
-    hydration: function(){
-        return this.water/this.flour*100;
-    }
+// const loaf = {
+//     flour: 300,
+//     water: 210,
+//     hydration: function(){
+//         return this.water/this.flour*100;
+//     }
+// }
+
+// console.log(loaf.flour);
+// console.log(loaf.water);
+
+// console.log(loaf.hydration());
+
+const obj = {
+    foo: 'fee',
+    bar: 'boo',
+    fum: 'fi',
+    quux: 'dog',
+    spam: 'email'
 }
 
-console.log(loaf.flour);
-console.log(loaf.water);
-
-console.log(loaf.hydration());
+for (key in obj){
+    console.log(`${key} ${obj[key]}`);
+}
