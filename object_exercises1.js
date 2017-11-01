8ce8ad7832ca796ed49ca8e065ac88573bc97486
Ryan Sabo <sabora920@gmail.com>
2017-11-01 11:57:42 -0400

    added challenge two

--- object_exercises1.js
+++ object_exercises1.js
@@ -1,44 +1,99 @@
-function createMyObject() {
-    return {
-    foo: 'bar',
-    answerToUniverse: 42,
-    'olly olly': 'oxen free',
-    sayHello: function() {
-        return 'hello';
-    }
-  }
+// function createMyObject() {
+//     return {
+//     foo: 'bar',
+//     answerToUniverse: 42,
+//     'olly olly': 'oxen free',
+//     sayHello: function() {
+//         return 'hello';
+//     }
+//   }
+// }
+
+// (function testCreateMyObject() {
+//     const obj = createMyObject();
+//     if (typeof obj !== 'object') { 
+//       console.error('ERROR: `createMyObject` must return an object');
+//       return false
+//     }
+//     const expectedKeys = ['foo', 'answerToUniverse', 'olly olly', 'sayHello'];
+//     expectedKeys.forEach(function(key) {
+//       if (!(key in obj)) {
+//         console.error(`ERROR: Missing a key for ${key}`);
+//         return false;
+//       }
+//     });
+//     if (obj.foo !== 'bar') {
+//       console.error(`ERROR: Value for foo should be 'bar' but was ${obj.foo}`);
+//       return false;
+//     }
+//     if (obj.answerToUniverse !== 42) {
+//       console.error(
+//         `ERROR: Value for \`answerToUniverse\` should be 42 but was ${obj.answerToUniverse}`);
+//       return false;
+//     }
+//     if (obj['olly olly'] !== 'oxen free') {
+//       console.error(
+//         `ERROR: Value for \`olly olly\` should be 'oxen free' but was ${obj['olly olly']}`);
+//       return false;
+//     }
+//     if (!(typeof obj.sayHello === "function" && obj.sayHello() === 'hello')) {
+//       console.error("ERROR: Value for `sayHello` must be a function that returns the string 'hello'");
+//       return false;
+//     }
+//     console.log('SUCCESS: Your function works!');
+//   })();
+
+
+
+
+
+
+function updateObject(obj) {
+  // your code here
+  obj.foo='foo';
+  obj.bar='bar';
+  obj.bizz='bizz';
+  obj.bang='bang';
+
+  return obj;
 }
 
-(function testCreateMyObject() {
-    const obj = createMyObject();
-    if (typeof obj !== 'object') { 
-      console.error('ERROR: `createMyObject` must return an object');
-      return false
-    }
-    const expectedKeys = ['foo', 'answerToUniverse', 'olly olly', 'sayHello'];
-    expectedKeys.forEach(function(key) {
-      if (!(key in obj)) {
-        console.error(`ERROR: Missing a key for ${key}`);
-        return false;
-      }
-    });
-    if (obj.foo !== 'bar') {
-      console.error(`ERROR: Value for foo should be 'bar' but was ${obj.foo}`);
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+(function testUpdateObject() {
+  const oldObj = {
+    cats: 'cats',
+    dogs: 'dogs',
+  };
+  const newObj = updateObject(oldObj);
+  if (typeof newObj !== 'object') { 
+    console.error('ERROR: `createMyObject` must return an object');
+    return false
+  }
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+    if (!(key in newObj)) {
+      console.error(`ERROR:  \`${key}\` not in object returned by \`updateObject\``);
       return false;
     }
-    if (obj.answerToUniverse !== 42) {
-      console.error(
-        `ERROR: Value for \`answerToUniverse\` should be 42 but was ${obj.answerToUniverse}`);
+  });
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+    if (newObj[key] !== key) {
+      console.error(`ERROR: \`${key}\` should be "${key}" but was ${newObj[key]}`);
       return false;
     }
-    if (obj['olly olly'] !== 'oxen free') {
-      console.error(
-        `ERROR: Value for \`olly olly\` should be 'oxen free' but was ${obj['olly olly']}`);
-      return false;
-    }
-    if (!(typeof obj.sayHello === "function" && obj.sayHello() === 'hello')) {
-      console.error("ERROR: Value for `sayHello` must be a function that returns the string 'hello'");
-      return false;
-    }
-    console.log('SUCCESS: Your function works!');
-  })();
+  });
+  if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
+    console.error("ERROR: your function doesn't preserve existing key/value pairs");
+    return false;
+  }
+  console.log('SUCCESS: `updateObject` works correctly!');
+  
+})();
